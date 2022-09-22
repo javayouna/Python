95af6957ba1194338599854ff8f0f22ebb50dda5
javayouna <106093094+javayouna@users.noreply.github.com>
2022-09-22 17:59:38 +0900

    Add files via upload

--- py/py1/bef6.py
+++ py/py1/bef6.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup  
 from requests import *
-
+#해당 사이트에 접속하여 Devtool로 볼 경우 데이터가 확인이 되지만 
+#실제 크롤링 후 스크래핑시 데이터가 확인이 안 될 경우는 AJAX 및 자바 스크립트로 직접 태그가 생성되도록 제작 되었음.
+#이럴경우 스크래핑 하기가 어려워짐
 url="https://www.koreabaseball.com/TeamRank/TeamRank.aspx"
 result=get(url) 
 result.raise_for_status()
---
+++ py/py1/dbconnect.py
@@ -0,0 +1,10 @@
+from sqlite3 import * #pip install pysqlite3
+from MySQLdb import * #database에 연결
+
+connect = connect(
+    user="moko2",
+    passwd="spdlxlqm2@",
+    host="umj7-009.cafe24.com",
+    db="moko2"
+    
+    )
