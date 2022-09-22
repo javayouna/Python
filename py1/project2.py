e87b10d96650c829f0c56dc1a14ec7d023bcdc69
javayouna <106093094+javayouna@users.noreply.github.com>
2022-09-22 17:58:39 +0900

    Delete py1 directory

--- py1/project2.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from asyncio.tasks import sleep
-
-dw = webdriver.Chrome(ChromeDriverManager().install())
-dw.get("https://www.naver.com")
-sleep(6000)
