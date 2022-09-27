62cdc2b40f70f51fd2b265f460005d79b89e1643
javayouna <106093094+javayouna@users.noreply.github.com>
2022-09-28 00:31:01 +0900

    Update portimg.py

--- portimg.py
+++ portimg.py
@@ -1,43 +1,52 @@
 from bs4 import BeautifulSoup  
 from requests import * 
 from random import *
-
+from re import * #정규식
 data="http://9s.co.kr/shop/shopbrand.html?xcode=127&type=Y"
-
 url=data
 html=get(url)
 html.raise_for_status()
-code=BeautifulSoup(html.text,"lxml")
-#code=BeautifulSoup(html.content.decode('euc-kr','replace'))
+html_text=html.text
+code=BeautifulSoup(html.content.decode('euc-kr','replace'),"lxml")
 products = code.find("ul",attrs={"class":"prd_wrap new2022"})
 lis=products.find_all("li")
 #print(lis)
 
-products_img=code.find('li','prd_list').find_all('img')
-print(products_img)
-prd_list=[]
-for f in products_img:
-    img_src1=f['src']
-    prd_list.append(img_src1)
-print(prd_list)
 
-
-'''
 for no in lis:
     products_subname =no.find("p",attrs={"class":"subname"}) #상품명
     products_name=no.find("p",attrs={"class":"name"}) #상품상세
-    products_subname2=no.find("span",attrs={"class":"subname2"}) #상품타입
-    products_strike =no.find("span",attrs={"class":"strike "})#정가
+    products_subname2=no.find("p",attrs={"class":"subname2"}) #상품타입
+    products_strike =no.find("p",attrs={"class":"strike"})#정가
+    products_strike =no.find("span",attrs={"class":"strike"})#정가
     products_price=no.find("span",attrs={"class":"price"})#할인가
-    products_MS_rod_img_s=no.find("img",attrs={"class":"MS_rod_img_s"})
-    #products_MS_rod_img_s=no.find("img")#이미지
-    #pd_img="https:"+products_MS_rod_img_s["lazy-load"]
+    products_MS_prod_img_s=no.find("img",attrs={"class":"MS_prod_img_s"})#이미지
+    products_src="http://9s.co.kr"+ products_MS_prod_img_s["src"]
+    #products_rollover="http://9s.co.kr"+ products_MS_prod_img_s["rollover_onimg"]
 
-    code=randrange(1000,9000) 
-
+    #상품명
     products_subname_tt=products_subname.get_text()
     products_subname_text=products_subname_tt.strip()
     #print(products_subname_text)
 
-    print(products_MS_rod_img_s)
-'''
+    #상품상세
+    products_name_tt=products_name.get_text()
+    products_name_text=products_name_tt.strip()
+    #print(products_name_text)
+
+    #상품타입
+    products_subname2_tt=products_subname2.get_text()
+    products_subname2_text=products_subname2_tt.strip()
+    print(products_subname2_text)
+   
+    #정가
+    products_strike_tt=products_strike.get_text()
+    products_strike_text=products_strike_tt.replace(",","")
+    #print(products_strike)
+    
+    #할인가
+    products_price_tt=products_price.get_text()
+
+
+    #이미지
+    #print(products_src)
